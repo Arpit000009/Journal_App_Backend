46653d1ce77a360919bc251cfc127a0c38f05ca5
Arpit000009 <arpitpandey000009@gmail.com>
2026-05-11 17:46:01 +0530

    this is first commit

---
+++ src/main/java/com/pro/journalApp/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.pro.journalApp.controller;
+
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
---
+++ src/main/java/com/pro/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.pro.journalApp.repository;
+
+import com.pro.journalApp.Entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
