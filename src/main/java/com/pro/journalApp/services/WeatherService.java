ca91f64a4edeb808e337c66fe1284ed4f3da1528
Arpit000009 <arpitpandey000009@gmail.com>
2026-07-15 05:35:14 +0530

    hiding keys

--- src/main/java/com/pro/journalApp/services/WeatherService.java
+++ src/main/java/com/pro/journalApp/services/WeatherService.java
@@ -2,16 +2,18 @@ package com.pro.journalApp.services;
 
 import com.pro.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
 @Service
 public class WeatherService {
-    private static final String apiKey = "90ed25a3eeeb85f921aef5ddc793afa7";
+
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
